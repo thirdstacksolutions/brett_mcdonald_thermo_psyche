7c0c5d33a906f50f817871bc55512ff1dd676bbb
AndreaRene <andrea@interhact.net>
2025-07-28 12:35:00 -0500

    updates: apply no context click to images and videos only. replace gear button with gear button shadow

--- assets/noclick.js
+++ assets/noclick.js
@@ -1,1 +1,10 @@
-window.addEventListener(`contextmenu`, (e) => e.preventDefault());
+document.addEventListener('contextmenu', function (e) {
+  const tag = e.target.tagName;
+  const isMedia = tag === 'IMG' || tag === 'VIDEO';
+  const isBgButton =
+    tag === 'BUTTON' && getComputedStyle(e.target).backgroundImage !== 'none';
+
+  if (isMedia || isBgButton) {
+    e.preventDefault();
+  }
+});
